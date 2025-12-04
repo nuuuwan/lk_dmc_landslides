c971fa435ed5873ce922cf78695cb74db7d6ebea
nuuuwan <nuuuwan@gmail.com>
2025-12-04 12:26:39 +0530

    Add test case for LandslideWarning from new PDF input and validate DSD IDs

--- tests/test_landslide_warning.py
+++ tests/test_landslide_warning.py
@@ -7,42 +7,27 @@ from lk_dmc import LandslideWarning
 class TestCase(unittest.TestCase):
 
     def test_from_pdf(self):
-        lw = LandslideWarning.from_pdf(
-            os.path.join("tests", "inputs", "2025-12-02.pdf"),
-            force_parse_pdf=True,
-        )
-        self.assertIsInstance(lw, LandslideWarning)
-        self.assertEqual(lw.date_id, "2025-12-02")
-        self.assertEqual(lw.get_level_to_n_warnings(), {1: 18, 2: 0, 3: 64})
 
-        kandy_level_3 = lw.level_to_district_to_dsds[3]["LK-21"]
-        self.assertEqual(
-            kandy_level_3,
-            [
-                "LK-2103",
-                "LK-2106",
-                "LK-2109",
-                "LK-2112",
-                "LK-2115",
-                "LK-2118",
-                "LK-2121",
-                "LK-2124",
-                "LK-2127",
-                "LK-2130",
-                "LK-2133",
-                "LK-2134",
-                "LK-2136",
-                "LK-2139",
-                "LK-2142",
-                "LK-2145",
-                "LK-2148",
-                "LK-2151",
-                "LK-2154",
-                "LK-2157",
-            ],
-        )
-        self.assertEqual(kandy_level_3, sorted(kandy_level_3))
+        for date_id, level_to_n_warnings, must_have_dsd_id_to_level in [
+            ["2025-12-03", {1: 23, 2: 26, 3: 38}, {"LK-8124": 2}],
+            ["2025-12-02", {1: 18, 2: 0, 3: 64}, {"LK-8127": 3}],
+        ]:
+            lw = LandslideWarning.from_pdf(
+                os.path.join("tests", "inputs", f"{date_id}.pdf"),
+                force_parse_pdf=True,
+            )
+            self.assertIsInstance(lw, LandslideWarning)
+            self.assertEqual(lw.date_id, date_id)
+            self.assertEqual(lw.get_level_to_n_warnings(), level_to_n_warnings)
 
+            for dsd_id, level in must_have_dsd_id_to_level.items():
+                district_id = dsd_id[:5]
+                dsd_ids = lw.level_to_district_to_dsds.get(level, {}).get(
+                    district_id, []
+                )
+                self.assertIn(dsd_id, dsd_ids)
+
+    @unittest.skip("Needs network access")
     def test_list_from_remote(self):
         n_limit = 10
         lw_list = LandslideWarning.list_from_remote(n_limit)
